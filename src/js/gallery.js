9e4926e959bfbe5d597f793410c9334a001ba50e
YevhenKharchenko <evgenvalerych@gmail.com>
2025-05-23 10:25:15 +0300

    Add reviews, gallery and play sections

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,7 +3,6 @@ import 'swiper/css/bundle';
 
 const galleryLeftArrow = document.getElementById('galleryLeftArrow');
 const galleryRightArrow = document.getElementById('galleryRightArrow');
-const galleryDots = document.querySelectorAll('.dot');
 
 let gallerySwiper;
 
@@ -12,6 +11,7 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
   loop: false,
   grabCursor: true,
   slidesPerView: 1,
+  centeredSlides: true,
   initialSlide: 0,
   spaceBetween: 20,
   grabCursor: true,
@@ -23,7 +23,8 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
   },
   breakpoints: {
     1440: {
-      slidesPerView: 4,
+      slidesPerView: 3,
+      initialSlide: 1,
     },
   },
   on: {
@@ -31,7 +32,6 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
       document.querySelector('.gallery-swiper-container').classList.add('show');
     },
     slideChange: function () {
-      updateGalleryDots(this.realIndex);
       updateGalleryArrows(this);
     },
   },
@@ -39,39 +39,10 @@ gallerySwiper = new Swiper('.gallery-swiper-container', {
 
 updateGalleryArrows(gallerySwiper);
 
-function updateGalleryDots(index) {
-  let dotIndex = 0;
-
-  if (window.innerWidth >= 1440) {
-    const totalSlides = gallerySwiper.slides.length;
-    const slidesPerView = gallerySwiper.params.breakpoints[1440].slidesPerView;
-
-    if (index === 0) {
-      dotIndex = 0;
-    } else if (index >= totalSlides - slidesPerView) {
-      dotIndex = 2;
-    } else {
-      dotIndex = 1;
-    }
-  } else {
-    dotIndex = index;
-  }
-
-  galleryDots.forEach((dot, i) => {
-    dot.classList.toggle('active', i === dotIndex);
-  });
-}
-
 function updateGalleryArrows(swiper) {
   galleryLeftArrow.disabled = swiper.isBeginning;
   galleryRightArrow.disabled = swiper.isEnd;
 }
-
-galleryDots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    gallerySwiper.slideTo(index);
-  });
-});
 
 galleryLeftArrow.addEventListener('click', () => {
   gallerySwiper.slidePrev();
